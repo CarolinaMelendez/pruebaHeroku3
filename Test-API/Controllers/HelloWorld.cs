efb02dd036b86b51972c6955a755836e07e2ed58
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2022-02-18 00:35:15 -0400

    prueba

---
+++ Test-API/Controllers/HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorld : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World¡");
+        }
+  
+    }
+}
+
